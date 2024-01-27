7d914e746e7696ad762c4ffb73d66493c8562dde
erhancekin <ummugulsumerdim@gmail.com>
2024-01-27 22:05:26 +0300

    Seed Customer Table

--- ShopiVerse/Data/ProjectDbContext.cs
+++ ShopiVerse/Data/ProjectDbContext.cs
@@ -17,7 +17,16 @@ namespace ShopiVerse.Data
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<Customer>().HasData(
-                new Customer { }
+    new Customer { Id = 1, Name = "Ali", Surname = "Yılmaz", City = "İstanbul", District = "Beşiktaş", Address = "Beşiktaş Mahallesi, No:1" },
+    new Customer { Id = 2, Name = "Ayşe", Surname = "Kaya", City = "Ankara", District = "Çankaya", Address = "Çankaya Mahallesi, No:2" },
+    new Customer { Id = 3, Name = "Mehmet", Surname = "Öz", City = "İzmir", District = "Bornova", Address = "Bornova Mahallesi, No:3" },
+    new Customer { Id = 4, Name = "Elif", Surname = "Demir", City = "Bursa", District = "Nilüfer", Address = "Nilüfer Mahallesi, No:4" },
+    new Customer { Id = 5, Name = "Ahmet", Surname = "Can", City = "Antalya", District = "Konyaaltı", Address = "Konyaaltı Mahallesi, No:5" },
+    new Customer { Id = 6, Name = "Zeynep", Surname = "Balcı", City = "Samsun", District = "İlkadım", Address = "İlkadım Mahallesi, No:6" },
+    new Customer { Id = 7, Name = "Kemal", Surname = "Yıldız", City = "Trabzon", District = "Ortahisar", Address = "Ortahisar Mahallesi, No:7" },
+    new Customer { Id = 8, Name = "Seda", Surname = "Akar", City = "Eskişehir", District = "Tepebaşı", Address = "Tepebaşı Mahallesi, No:8" },
+    new Customer { Id = 9, Name = "Murat", Surname = "Tekin", City = "Kocaeli", District = "İzmit", Address = "İzmit Mahallesi, No:9" },
+    new Customer { Id = 10, Name = "Esra", Surname = "Kurt", City = "Gaziantep", District = "Şahinbey", Address = "Şahinbey Mahallesi, No:10" }
                 );
         }
 }
---
+++ ShopiVerse/Migrations/20240127190431_SeedCustomerTable.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace ShopiVerse.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedCustomerTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Customers",
+                columns: new[] { "Id", "Address", "City", "District", "Name", "Surname" },
+                values: new object[,]
+                {
+                    { 1, "Beşiktaş Mahallesi, No:1", "İstanbul", "Beşiktaş", "Ali", "Yılmaz" },
+                    { 2, "Çankaya Mahallesi, No:2", "Ankara", "Çankaya", "Ayşe", "Kaya" },
+                    { 3, "Bornova Mahallesi, No:3", "İzmir", "Bornova", "Mehmet", "Öz" },
+                    { 4, "Nilüfer Mahallesi, No:4", "Bursa", "Nilüfer", "Elif", "Demir" },
+                    { 5, "Konyaaltı Mahallesi, No:5", "Antalya", "Konyaaltı", "Ahmet", "Can" },
+                    { 6, "İlkadım Mahallesi, No:6", "Samsun", "İlkadım", "Zeynep", "Balcı" },
+                    { 7, "Ortahisar Mahallesi, No:7", "Trabzon", "Ortahisar", "Kemal", "Yıldız" },
+                    { 8, "Tepebaşı Mahallesi, No:8", "Eskişehir", "Tepebaşı", "Seda", "Akar" },
+                    { 9, "İzmit Mahallesi, No:9", "Kocaeli", "İzmit", "Murat", "Tekin" },
+                    { 10, "Şahinbey Mahallesi, No:10", "Gaziantep", "Şahinbey", "Esra", "Kurt" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Customers",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Customers",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Customers",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Customers",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Customers",
+                keyColumn: "Id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Customers",
+                keyColumn: "Id",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                table: "Customers",
+                keyColumn: "Id",
+                keyValue: 7);
+
+            migrationBuilder.DeleteData(
+                table: "Customers",
+                keyColumn: "Id",
+                keyValue: 8);
+
+            migrationBuilder.DeleteData(
+                table: "Customers",
+                keyColumn: "Id",
+                keyValue: 9);
+
+            migrationBuilder.DeleteData(
+                table: "Customers",
+                keyColumn: "Id",
+                keyValue: 10);
+        }
+    }
+}
