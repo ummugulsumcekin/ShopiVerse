446793e00a13ae171dcf383dac52563717679424
erhancekin <ummugulsumerdim@gmail.com>
2024-01-28 00:55:46 +0300

    CREATE Customer and gett all customer list

--- ShopiVerse/Controllers/CustomerController.cs
+++ ShopiVerse/Controllers/CustomerController.cs
@@ -1,12 +1,34 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using ShopiVerse.Data;
+using ShopiVerse.Models;
+using System.Collections.Generic;
+using System.Reflection.Metadata.Ecma335;
 
 namespace ShopiVerse.Controllers
 {
     public class CustomerController : Controller
     {
+        private readonly ProjectDbContext _db;
+        public CustomerController(ProjectDbContext db)
+        {
+            _db = db;
+        }
         public IActionResult Index()
         {
+            List<Customer> objCustomerList = _db.Customers.ToList();
+            return View(objCustomerList);
+        }
+        [HttpPost]
+        public IActionResult Create(Customer obj)
+        {
+            if (ModelState.IsValid) 
+            { 
+            _db.Customers.Add(obj);
+            _db.SaveChanges();
+             return RedirectToAction("Index");
+            }
             return View();
         }
+        
     }
 }
